d1d406b9fb59d45b9f39d77803af2564cd0cc6c0
cc_mis <cc_mis@csawp-vmora04.ok.aero>
2016-09-26 15:39:59 +0200

    x

--- cutcsv.go
+++ cutcsv.go
@@ -1,294 +1,257 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/csv"
+	"io"
 	"io/ioutil"
 	"log"
-	"strconv"
-	"gopkg.in/yaml.v2"
-	"encoding/csv"
+	"os"
 	"path"
 	"regexp"
-	"os"
-	"io"
-	"unicode/utf8"
+	"strconv"
 	"strings"
+	"text/template"
+	"unicode/utf8"
+
+	"cutcsv/tpl"
+
 	"github.com/olekukonko/tablewriter"
-	)
-type Field_cfgT struct{
-		Default  map[string]string `yaml:",omitempty"`
-		Match	 map[string]string `yaml:",omitempty"`
-		Printf   map[string][]string `yaml:",omitempty"`
-		Type   map[string]string `yaml:",omitempty"`
-		Case   map[string]map[string]string `yaml:",omitempty"`
-	}	
-type FileGrpT struct{
-	Match, Delimiter, Comment string `yaml:",omitempty"`
-    Trim, Time string `yaml:",omitempty"`
-	Skip int `yaml:",omitempty"`
-	Field Field_cfgT
-	File []struct{
-		Match, Delimiter, Comment string `yaml:",omitempty"`
-		Trim, Time string `yaml:",omitempty"`
-		Skip int `yaml:",omitempty"`
-		Fields []string `yaml:",omitempty"`
-		Field Field_cfgT
-		Pipe []struct{
-			Line []map[string][]string `yaml:",omitempty"`
-		}
-	}
+	"gopkg.in/yaml.v2"
+)
+
+//InputMatch ...
+type InputFile struct {
+	Match        string            `yaml:",omitempty" short:"m" long:"matchFile"`
+	Delimiter    string            `yaml:",omitempty" short:"d" long:"delimiter" default:","`
+	Comment      string            `yaml:",omitempty"`
+	Trim         string            `yaml:",omitempty"`
+	Time         string            `yaml:",omitempty"`
+	Skip         int               `yaml:",omitempty" long:"skip"`
+	Templates    map[string]string `yaml:",omitempty"`
+	Fields       map[string]string `yaml:",omitempty" short:"f" long:"field"`
+	SourceFields []string          `yaml:",omitempty"`
 }
 
-
-var cfg []FileGrpT
-
-type FieldT struct{
-				index int
-				ftype string
-				printf []string
-				defaul string
-				value string
-				regex *regexp.Regexp
-				submatch []string
-				cases map[string]string
-		}
-
-
-type ReaderCSV struct{
-	FileName string
-	FilePath string
-	Delimiter string
-	Comment string
-	Skip int
-	Fields map[string]FieldT
-	Match map[string]FieldT
-	Type map[string]FieldT
-	Default map[string]FieldT
-	Printf map[string]FieldT
-	Case map[string]FieldT
-	Record []string
-	RecordSet [][]string
-	Reader *csv.Reader
+//ReaderCSV ...
+type ReaderCSV struct {
+	input     string
+	filePath  string
+	trim      string
+	delimiter rune
+	comment   string
+	skip      int
+	fieldMap  map[string]field
+	valueMap  map[string]interface{}
+	record    []string
+	line      int
+	recordSet [][]string
+	reader    *csv.Reader
 }
 
-func read_conf_file(fp string) {
- cfg_src, _ := ioutil.ReadFile(fp)
- err := yaml.Unmarshal(cfg_src, &cfg)
- if err != nil {
-   log.Fatalf("error: %v", err)
- }
+type field struct {
+	sourceType  string
+	sourceIndex int
+	template    *template.Template
 }
 
-func nvlint (p1, p2 int) int {
-	if p1 != 0 {
-		return p1
-	} else {
-		return p2
-	}
-}
-func nvl (p1, p2 string) string {
-	if p1 != "" {
-		return p1
-	} else {
-		return p2
-	}
-}
-func NewReaderCSV(fi io.Reader, fp string) *ReaderCSV{
- fn := path.Base(fp)
- for j, c := range cfg {
-	if matched, _ := regexp.MatchString(c.Match, fn); !matched  {
-		continue
-	}
-	 for _, file := range cfg[j].File {
-		if matched, _ := regexp.MatchString(file.Match, fn); !matched  {
-			continue
-		}
-		fm := make(map[string]FieldT)
-		mm := make(map[string]FieldT)
-		pm := make(map[string]FieldT)
-		dm := make(map[string]FieldT)
-		tm := make(map[string]FieldT)
-		cm := make(map[string]FieldT)
-		for i, f := range file.Fields {
-			fm[string(f)] = FieldT{index: i, ftype: "string"}
-		}
-		// definice poli mame na urovni skupiny a prednostne souboru
-		for _, def := range []Field_cfgT{c.Field, file.Field} {
-
-			// v fm mame vsechna pole, duplicitne pak pro Default,..
-			for k, v := range def.Default {
-				i, ok := fm[k]
-				if ok {
-					i.defaul = v
-					fm[k] = i
-				} else {
-					fm[k] = FieldT{index: len(fm) , ftype: "string", defaul: v}
-				}
-				dm[k] = fm[k]
-			}
-
-			for k, v := range def.Match {
-				i, ok := fm[k]
-				if ok {
-					i.regex = regexp.MustCompile(v)
-					fm[k] = i
-				} else {
-					fm[k] = FieldT{index: len(fm) , ftype: "string", regex: regexp.MustCompile(v)}
-				}
-				mm[k] = fm[k]
-			}
-
-			for k, v := range def.Printf {
-				i, ok := fm[k]
-				if ok {
-					i.printf = v
-					fm[k] = i
-				} else {
-					fm[k] = FieldT{index: len(fm) , ftype: "string", printf: v}
-				}
-				pm[k] = fm[k]
-			}
-				for k, v := range def.Type {
-				i, ok := fm[k]
-				if ok {
-					i.ftype = v
-					fm[k] = i
-				} else {
-					fm[k] = FieldT{index: len(fm) , ftype: v}
-				}
-				tm[k] = fm[k]
-			}
-
-			for k, v := range def.Case {
-				i, ok := fm[k]
-				if ok {
-					i.cases = v
-					fm[k] = i
-				} else {
-					fm[k] = FieldT{index: len(fm) , ftype: "string", cases: v}
-				}
-				cm[k] = fm[k]
-			}
-
-		}
-		r := csv.NewReader(fi)
-		ru, _ := utf8.DecodeRuneInString( nvl(c.Delimiter, file.Delimiter ))
-		r.Comma = ru
-		r.FieldsPerRecord = -1
-		r.TrimLeadingSpace = true
-
-		return &ReaderCSV{
-			FileName: fn,
-			FilePath: fp,
-			Comment:  nvl(c.Comment, file.Comment ),
-			Skip: nvlint(c.Skip, file.Skip),
-			Fields: fm,
-			Match: mm,
-			Default: dm,
-			Printf: pm,
-			Type: tm,
-			Case: cm,
-			Reader: r,
-
-		}
-	 }
- }
- return nil
+//WriterCSV ...
+type WriterCSV struct {
+	writer *csv.Writer
+	limit  int
+	line   int
 }
 
-func atoi( a string) int {
-	i, _ := strconv.Atoi(a)
-	return i
-}
-func (r *ReaderCSV) Query(fields ...string) [][]string {
-	indexes := make([]int, len(fields))
-	for i, f := range fields {
-		indexes[i] = r.Fields[f].index 
-	}
-	var rcs [][]string
-	for _, row := range r.RecordSet {
-		rc := make([]string, len(indexes))
-		for i, index := range indexes {
-			rc[i] = row[index]
-		}
-		rcs = append(rcs, rc)
-	}
-	return rcs
-	
-}
-func (r *ReaderCSV) read() {
-i := 0
-rx := regexp.MustCompile(`\[(\w+)\](?:\[(\d+)\])*`)
-      for {
-        rc, err := r.Reader.Read()
-        if err == io.EOF  {
-            break
-        }
-        if err != nil {
-            log.Fatal(err)
-        }
-		i++
-			
-        if rc[0] == r.Comment || i <= r.Skip {
-            continue
-        }
+//ReadConfig ...
+func ReadConfig(fp string) []InputMatch {
 
-		//doplnime pridana vlastni pole
-		if l := len(r.Fields) - len(rc); l > 0 {
-			rc = append(rc, make([]string, l)...)
-		}
-
-		for _, v := range r.Default {
-			if rc[v.index] == "" {
-				rc[v.index] = v.defaul
-			}
-		}
-
-		for k, v := range r.Match {
-			v.submatch = v.regex.FindStringSubmatch(rc[v.index])
-			r.Match[k] = v
-		}
-
-		for _, v := range r.Printf {
-			for _, y := range v.printf[1:] {
-			
-			x :=  rx.FindStringSubmatch(y) // vraci pole 3 lementu 
-			//varianty %[SMOD] nebo %[SMOD][0]
-			if len(x) == 3 && x[2] != "" {
-				q := strings.Replace(y, x[0], r.Match[x[1]].submatch[atoi(x[2])], -1)
-				rc[v.index] = fmt.Sprintf(v.printf[0], q)
-			} else if len(x) == 3 && x[2] == "" {
-				q := strings.Replace(y, x[0], rc[r.Fields[x[1]].index], -1)
-				rc[v.index] = fmt.Sprintf(v.printf[0], q)
-			}
-			}
-			
-		}
-
-		for _, v := range r.Case {
-			rc[v.index] = v.cases[rc[v.index]]
-			//fmt.Println(v.index)
-		}
-
-		r.Record = rc
-		r.RecordSet = append(r.RecordSet , rc)
-		//fmt.Println(rc)
-
-	}
-}
-func main() {
-	fp := "STAT_ISSUE_FOP20160818NJOB49_1607.DAT"
-	read_conf_file("edgar_csv.yml")
-	fi, err := os.Open(fp)
+	src, err := ioutil.ReadFile(fp)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer  fi.Close()
+	var cfg []InputMatch
+	err = yaml.Unmarshal(src, &cfg)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	return cfg
+}
 
-	r := NewReaderCSV(fi,fp) 
-	r.read()
-	table := tablewriter.NewWriter(os.Stdout)
-	table.AppendBulk(r.Query("RECTYPE", "FLAG", "TACN", "TDNR", "DAIS", "TYPDOC", "AGTN", "PNR", "PNRA"))
-	table.Render()
-	
-}	
+func nvlint(p1, p2 int) int {
+	if p1 != 0 {
+		return p1
+	}
+	return p2
+}
+func nvl(p1, p2 string) string {
+	if p1 != "" {
+		return p1
+	}
+	return p2
+}
+
+// NewWriterCSV ---
+func NewWriterCSV(w io.Writer) *WriterCSV {
+	return &WriterCSV{
+		writer: csv.NewWriter(os.Stdout),
+		limit:  -1,
+	}
+}
+
+// NewReaderCSV ---
+func NewReaderCSV(reader io.Reader, input string, cfg []InputMatch) *ReaderCSV {
+	rCSV := &ReaderCSV{
+		delimiter: ',',
+		skip:      0,
+		fieldMap:  make(map[string]field),
+		valueMap:  make(map[string]interface{}),
+	}
+	tpl.Init()
+	tmpl := template.New("master").Funcs(tpl.FuncMap)
+	for _, c := range cfg {
+		if matched, _ := regexp.MatchString(c.Match, input); !matched {
+			continue
+		}
+		if c.Delimiter != "" {
+			rCSV.delimiter, _ = utf8.DecodeRuneInString(c.Delimiter)
+		}
+		if c.Trim != "" {
+			rCSV.trim = c.Trim
+		}
+		if c.Comment != "" {
+			rCSV.comment = c.Comment
+		}
+		if c.Skip > 0 {
+			rCSV.skip = c.Skip
+		}
+		for i, v := range c.SourceFields {
+			s := strings.Split(v, ".")
+			f, ok := rCSV.fieldMap[s[0]]
+			if ok == false {
+				f = field{sourceIndex: i}
+			} else {
+				f.sourceIndex = i
+			}
+			if len(s) > 1 {
+				f.sourceType = s[1]
+			} else {
+				f.sourceType = "string"
+			}
+			rCSV.fieldMap[s[0]] = f
+			rCSV.valueMap[s[0]] = ""
+		}
+		for k, v := range c.Templates {
+			_ = template.Must(tmpl.New(k).Parse(v))
+		}
+		for k, v := range c.Fields {
+			t := template.Must(tmpl.New(k).Parse(v))
+			if f, ok := rCSV.fieldMap[k]; ok == false {
+				rCSV.fieldMap[k] = field{template: t, sourceIndex: -1}
+			} else {
+				f.template = t
+				rCSV.fieldMap[k] = f
+			}
+		}
+	}
+	r := csv.NewReader(reader)
+	r.FieldsPerRecord = -1
+	if strings.Contains(rCSV.trim, "L") {
+		r.TrimLeadingSpace = true
+	}
+	r.Comma = rCSV.delimiter
+	rCSV.reader = r
+
+	return rCSV
+}
+
+func atoi(a string) int {
+	i, _ := strconv.Atoi(a)
+	return i
+}
+
+//Query ...
+func (r *ReaderCSV) Query(w *WriterCSV, outFields ...string) {
+	r.line = 0
+	w.line = 0
+	for {
+		if w.limit > -1 && w.line >= w.limit {
+			break
+		}
+		var rcOut []string
+		rcIn, err := r.reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		r.line++
+		if strings.HasPrefix(rcIn[0], r.comment) || r.line <= r.skip {
+			continue
+		}
+
+		for k := range r.valueMap {
+			if i := r.fieldMap[k].sourceIndex; i >= 0 && i < len(rcIn) {
+				if strings.Contains(r.trim, "R") { // L nam resi csv.reader
+					rcIn[i] = strings.TrimRight(rcIn[i], " ")
+				}
+				switch r.fieldMap[k].sourceType {
+				case "Int()":
+					if s, err := strconv.Atoi(rcIn[i]); err == nil {
+						r.valueMap[k] = s
+					}
+				case "Float()":
+					if s, err := strconv.ParseFloat(rcIn[i], 64); err == nil {
+						r.valueMap[k] = s
+					}
+				default:
+					r.valueMap[k] = rcIn[i]
+				}
+			}
+		}
+
+		for _, v := range outFields {
+			if tmpl := r.fieldMap[v].template; tmpl != nil {
+				var buf bytes.Buffer
+				err := tmpl.Execute(&buf, r.valueMap)
+				if err != nil {
+					log.Println("executing template:", err)
+				}
+				rcOut = append(rcOut, buf.String())
+			} else if i := r.fieldMap[v].sourceIndex; i >= 0 && i < len(rcIn) {
+				rcOut = append(rcOut, rcIn[i])
+
+			}
+		}
+		//rsetOut = append(rsetOut, rcOut)
+		w.line++
+		if err := w.writer.Write(rcOut); err != nil {
+			log.Fatalln("error writing record to csv:", err)
+		}
+	}
+	// Write any buffered data to the underlying writer (standard output).
+	w.writer.Flush()
+
+	if err := w.writer.Error(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+
+	input := "STAT_ISSUE_CPNI20160920NJOB49_1608.DAT"
+	cfg := ReadConfig("edgar_csv.yaml")
+	//log.Printf("%+v", cfg)
+	fi, err := os.Open(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fi.Close()
+
+	r := NewReaderCSV(fi, path.Base(input), cfg)
+	w := NewWriterCSV(os.Stdout)
+	w.limit = 10
+	//log.Printf("%+v", r)
+	r.Query(w, "SQNU", "RECTYPE", "TACN", "TDNR", "DAIS", "TYPDOC", "AGTN", "FAR_CZK")
+	_ = tablewriter.NewWriter(os.Stdout)
+}
